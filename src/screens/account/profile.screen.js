9e7e97b0ba2c2905a3e4998bd526eb7bc1408a38
shynnhuy <shynn@hotmail.com>
2021-09-25 17:33:50 +0700

    feat: add upload avatar feature

--- src/screens/account/profile.screen.js
+++ src/screens/account/profile.screen.js
@@ -1,14 +1,55 @@
 import React from "react";
-import {Button, Image, StyleSheet, View} from "react-native";
+import {Button, Image, StyleSheet, View, Pressable} from "react-native";
 import {ScrollView} from "react-native-gesture-handler";
 import {useDispatch, useSelector} from "react-redux";
+import {launchCamera, launchImageLibrary} from "react-native-image-picker";
 import {actionUpdateInfo} from "../../redux/auth";
 import {UserForm} from "./components/UserForm";
+import {api} from "../../api";
+
+const createFormData = (photo, body = {}) => {
+  const data = new FormData();
+
+  data.append("image", {
+    name: photo.fileName,
+    type: photo.type,
+    uri: Platform.OS === "ios" ? photo.uri.replace("file://", "") : photo.uri,
+  });
+  Object.keys(body).forEach((key) => {
+    data.append(key, body[key]);
+  });
+  return data;
+};
 
 export const ProfileScreen = () => {
   const auth = useSelector(state => state.auth);
   const dispatch = useDispatch();
   const updateInfo = data => dispatch(actionUpdateInfo(data));
+
+  const [photo, setPhoto] = React.useState(null);
+
+  const handleChoosePhoto = () => {
+    // launchCamera({}, console.log);
+    launchImageLibrary({noData: true}, response => {
+      console.log(response.assets[0]);
+      if (response) {
+        setPhoto(response.assets[0]);
+      }
+    });
+  };
+
+  const handleUploadPhoto = async () => {
+    try {
+      const res = await api.patch(
+        "/auth/avatar",
+        createFormData(photo, {userId: auth.user._id}),
+      );
+      console.log("DATA: ", res);
+    } catch (error) {
+      console.log("ERROR: ", error.data);
+    }
+  };
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <View
@@ -19,15 +60,26 @@ export const ProfileScreen = () => {
         <View
           style={{
             alignItems: "center",
+            justifyContent: "center",
             marginBottom: 15,
           }}>
-          <View style={styles.avatar}>
+          <Pressable onPress={handleChoosePhoto} style={styles.avatar}>
             <Image
-              source={require("../../assets/splash.png")}
+              source={
+                Boolean(photo)
+                  ? {uri: photo.uri}
+                  : require("../../assets/splash.png")
+              }
               style={styles.image}
             />
-          </View>
-          <Button style={styles.avatarBtn} title="Change profile picture" />
+          </Pressable>
+          {Boolean(photo) && (
+            <Button
+              onPress={handleUploadPhoto}
+              style={styles.avatarBtn}
+              title="Upload Image"
+            />
+          )}
         </View>
         <UserForm {...auth.user} onSubmit={updateInfo} />
       </View>
